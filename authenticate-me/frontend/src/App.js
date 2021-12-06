3c06be8db900e904f71b8f3d7396d3cf3515898b
droid97 <andresaguilarbazar@gmail.com>
2021-12-06 01:08:36 -0600

    login form

--- authenticate-me/frontend/src/App.js
+++ authenticate-me/frontend/src/App.js
@@ -1,6 +1,22 @@
+import React, { useState, useEffect } from "react";
+
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <div className="main-container">
+
+        <Switch>
+          <Route path="/login">
+            <LoginFormPage />
+          </Route>
+
+        </Switch>
+
+
+
+    </div>
   );
 }
 
