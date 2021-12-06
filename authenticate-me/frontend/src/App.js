71bc49e38f30a663836ee0dc9d612c4724f47bc1
droid97 <andresaguilarbazar@gmail.com>
2021-12-06 01:48:51 -0600

    signup

--- authenticate-me/frontend/src/App.js
+++ authenticate-me/frontend/src/App.js
@@ -1,22 +1,26 @@
 import React, { useState, useEffect } from "react";
-
+import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
+import * as sessionActions from "./store/session";
 
 function App() {
-  return (
-    <div className="main-container">
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
 
-        <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
-
-        </Switch>
-
-
-
-    </div>
+  return isLoaded && (
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+      <Route path="/signup">
+        <SignupFormPage />
+      </Route>
+    </Switch>
   );
 }
 
